579987bad5c30ad610fcaa6aff6b34e51eb65538
Lonejyc <jocelyn.marcilloux@gmail.com>
2025-02-08 16:52:26 +0100

    feat: add pres images

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
-import Head from "/public/images/head.png";
+import Head1 from "/public/images/head1.png";
+import Head2 from "/public/images/head2.png";
 import Illustrator from "/public/images/illustrator.png";
 import After from "/public/images/afterEffect.png";
-import Arrow from "/public/images/arrow.svg";
+import Arrow1 from "/public/images/arrow1.svg";
+import Arrow2 from "/public/images/arrow2.svg";
+import Arrow3 from "/public/images/arrow3.svg";
+import Hashtag from "/public/images/hashtag.svg";
 
 export default async function Home() {
   return (
@@ -11,10 +15,10 @@ export default async function Home() {
         <h1 className="text-white text-9xl text-center mx-32 wide">PORTFOLIO DYLAN THOMAS</h1>
         <div className="elements">
           <div className="head">
-            <Image src={Head} alt="Tête de Dylan Thomas"></Image>
+            <Image src={Head1} alt="Tête de Dylan Thomas"></Image>
           </div>
           <div className="head">
-            <Image src={Head} alt="Tête de Dylan Thomas"></Image>
+            <Image src={Head1} alt="Tête de Dylan Thomas"></Image>
           </div>
           <div className="logo">
             <Image src={After} alt="Logo de After Effect" className="logoEL shadow-md rounded-xl"></Image>
@@ -24,12 +28,12 @@ export default async function Home() {
           </div>
         </div>
         <a href="#" className="navArrow bg-white rounded-full flex justify-center items-center w-14 h-14 absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-300 hover:translate-y-0.5">
-          <Image src={Arrow} alt="Flèche vers le bas"></Image>
+          <Image src={Arrow1} alt="Flèche vers le bas"></Image>
         </a>
       </section>
       <section className="pres rounded-t-xxxl bg-white text-black py-20 flex gap-16">
-        <div className="content w-1/2 pl-40">
-          {/* <Image src={} alt=""></Image> */}
+        <div className="content w-1/2 pl-40 relative">
+          <Image src={Arrow2} alt="Flèche tordu" className="absolute top-0 left-0"></Image>
           <h2 className="mb-10 soulmaze">Présentation</h2>
           <p className="pb-10">
               🎨 Salut, moi c’est Dylan !
@@ -40,14 +44,16 @@ export default async function Home() {
               <br /><br />
               Que ce soit pour donner vie à une marque, booster une communication ou simplement éviter un logo qui ressemble à un PowerPoint des années 2000, je suis là !
           </p>
-          <a href="#" className="bg-dGreen px-16 py-3 uppercase text-white rounded-full soulmaze">La suite</a>
-          {/* <Image src={} alt=""></Image> */}
+          <div className="flex items-center">
+            <a href="#" className="bg-dGreen px-16 py-3 uppercase text-white rounded-full soulmaze">La suite</a>
+            <Image src={Arrow3} alt="Flèche tordu" className="ml-6 translate-y-1/3"></Image>
+          </div>
         </div>
-        <div className="images w-1/2 pr-40">
-        <div className="image bg-dGray rounded-xxl shadow-md px-28 py-24">
-          <Image src={Head} alt="Tête de Dylan Thomas"></Image>
-        </div>
-          {/* <Image src={} alt=""></Image> */}
+        <div className="images w-1/2 pr-40 relative">
+          <div className="image bg-dGray shadow-md mt-8">
+            <Image src={Head2} alt="Tête de Dylan Thomas"></Image>
+            <Image src={Hashtag} alt="Hashtag" className="absolute right-0 bottom-0 translate-y-1/2 translate-x-1/3"></Image>
+          </div>
         </div>
       </section>
     </main>
